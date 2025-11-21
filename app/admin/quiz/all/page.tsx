2b27cb8a9de6f43b785c5f14457d8a339f03321c
Rehaman Syed <rehamansyed07@gmail.com>
2025-11-21 23:03:27 +0530

    Remove QuizManager import from AllQuizzesPage

--- app/admin/quiz/all/page.tsx
+++ app/admin/quiz/all/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import AdminAuthGuard from '@/app/components/admin/AdminAuthGuard';
 import AdminLayout from '@/app/components/admin/AdminLayout';
-import QuizManager from '@/app/components/admin/QuizManager';
+
 
 export default function AllQuizzesPage() {
   return (
